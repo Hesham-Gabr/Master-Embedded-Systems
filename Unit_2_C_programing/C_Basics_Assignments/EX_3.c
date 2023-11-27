3f14c64e31137e2ea5db95f5d7a2e7e9a008f1d1
Hesham Amir <heshamgabr2001@gmail.com>
2023-11-27 19:25:19 +0200

    Unit_2

---
+++ Unit_2_C_programing/C_Basics_Assignments/EX_3.c
@@ -0,0 +1,19 @@
+/*
+ * EX_3.c
+ *
+ *  Created on: Nov 27, 2023
+ *      Author: hesha
+ */
+#include <stdio.h>
+
+void main()
+{
+	int x = 0, y = 0;
+	printf("Enter Two Integers : ");
+	fflush(stdout);
+	fflush(stdin);
+	scanf("%d %d",&x, &y);
+	printf("Sum = %d",x+y);
+
+}
+
---
+++ Unit_2_C_programing/C_Basics_Assignments/EX_4.c
@@ -0,0 +1,19 @@
+/*
+ * EX_4.c
+ *
+ *  Created on: Nov 27, 2023
+ *      Author: hesham
+ */
+#include <stdio.h>
+
+void main()
+{
+	float x = 0, y = 0;
+	printf("Enter Two Numbers : ");
+	fflush(stdin);
+	fflush(stdout);
+	scanf("%f %f", &x, &y);
+	printf("product : %f",x * y);
+}
+
+
